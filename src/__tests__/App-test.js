c2261e4fd9cc63dff711d6c01c804b1109f081cf
byronkamal <byronkamal@hotmail.com>
2020-10-25 00:34:21 -0300

    Configuração do jest no projeto

--- src/__tests__/App-test.js
+++ src/__tests__/App-test.js
@@ -6,9 +6,12 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
+import {expect, jest, test} from '@jest/globals';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+jest.useFakeTimers();
+it('renders correctly', async () => {
   renderer.create(<App />);
 });
